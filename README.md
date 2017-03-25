c4503b3ead4eea18767fe6d578d42601791cebb9
Amol Kailash <Amol.Kailash@gmail.com>
2017-03-25 12:28:44 +0000

    Docker v 1.12 works with Docker Hello-World

---
+++ README.md
@@ -0,0 +1,27 @@
+# Running various Docker versions on Centos7 x64 Vagrant Virtual Box VM
+
+*Used on Mac OS X Sierra 10.12.3*
+
+## Software Platforms version
+1. Vagrant version 1.9.1
+2. Virtual Box version 5.1.12
+
+
+## Install Virtual Box Guest Additions plugin
+
+The official Centos7 Vagrant Box does not come with virtualbox Guest Addtions installed.
+
+There is a Vagrant Plugin however which will automatically do this for you.
+Run command:
+```
+vagrant plugin install vagrant-vbguest
+```
+
+
+## Commands to run
+
+```
+Run vagrant up
+vagrant ssh
+sudo docker run hello-world
+```
---
+++ install_docker_1_12.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+#sudo yum install -y epel-release 
+#sudo yum install -y net-tools python-setuptools python-devel libffi-devel openssl-devel #libssl-dev 
+#sudo yum groupinstall "Development Tools" -y
+#
+echo Installing Docker Version 1.12........
+
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum install -y docker-engine-1.12.1-1.el7.centos
+
+
+sudo usermod -a -G docker vagrant
+
+sed -i 's/^%wheel\tALL=(ALL)\tALL/%wheel\tALL=(ALL)\tNOPASSWD: ALL/g' /etc/sudoers 
+
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo curl -L "https://github.com/docker/compose/releases/download/2.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
