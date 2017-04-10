0f8dd6120a4e2a42ec80a0c7d553ffb3305cb32b
Amol Kailash <Amol.Kailash@gmail.com>
2017-04-10 22:21:56 +0100

    Fix Docker Compose version in download URI

--- install_docker_1_12.sh
+++ install_docker_1_12.sh
@@ -24,6 +24,6 @@ sed -i 's/^%wheel\tALL=(ALL)\tALL/%wheel\tALL=(ALL)\tNOPASSWD: ALL/g' /etc/sudoe
 
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo curl -L "https://github.com/docker/compose/releases/download/2.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
